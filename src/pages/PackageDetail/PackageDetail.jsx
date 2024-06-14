dd1552e36588e625fb5b4fd843872823ca027903
hyojeong <nano0912d@gmail.com>
2024-06-14 15:30:20 +0900

    Design: 상품 상세페이지

--- src/pages/PackageDetail/PackageDetail.jsx
+++ src/pages/PackageDetail/PackageDetail.jsx
@@ -1,13 +1,15 @@
 import styles from './PackageDetail.module.css';
+import PackageInformation from './PackageInformation/PackageInformation';
 import AirplainInfomation from './AirplainInformation/AirplainInformation';
 import Schedule from './Schedule/Schedule';
 
 const PackageDetail = () => {
   return (
-    <>
+    <div className={`${styles.PackageDetail} layout`}>
+      <PackageInformation />
       <AirplainInfomation />
       <Schedule />
-    </>
+    </div>
   );
 };
 
--- src/pages/PackageDetail/Schedule/Schedule.jsx
+++ src/pages/PackageDetail/Schedule/Schedule.jsx
@@ -1,7 +1,12 @@
 import styles from '../PackageDetail.module.css';
 
 const Schedule = () => {
-  return <div></div>;
+  return (
+    <section className={styles.Schedule}>
+      <h3>여행 일정</h3>
+      <div></div>
+    </section>
+  );
 };
 
 export default Schedule;
