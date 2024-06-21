74df0d1e4f1f59b5b242eb10a7344852c5e19699
hyojeong <nano0912d@gmail.com>
2024-06-21 15:00:24 +0900

    Feat: 응원하기

--- src/pages/DiyDetail/Description/Description.jsx
+++ src/pages/DiyDetail/Description/Description.jsx
@@ -1,6 +1,22 @@
 import styles from '../DiyDetail.module.css';
+import axios from 'axios';
 
 const Description = () => {
+  const postCheering = async () => {
+    try {
+      const response = axios.post(`/api/packages/likes/1`, {
+        userNum: 3,
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.log(`에러 발생: ${error}`);
+    }
+  };
+
+  const onClickCheering = () => {
+    postCheering();
+  };
+
   return (
     <>
       <div className={styles.diy_thumbnail}>
@@ -9,11 +25,8 @@ const Description = () => {
       <h2>베트남으로 떠나는 힐링 여행</h2>
       <p>응원하기 눌러주세요~~~~~</p>
       <div className={styles.cheering}>
-        <button>응원하기</button>
-        <div>
-          <span>1</span>
-          <span>2</span>
-        </div>
+        <button onClick={onClickCheering}>응원하기</button>
+        <div>12</div>
       </div>
     </>
   );
