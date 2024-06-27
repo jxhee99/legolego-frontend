1be04e92eaeb64a81d055643ecb2c7c7539578e0
hyojeong <nano0912d@gmail.com>
2024-06-27 12:16:37 +0900

    Feat: notfound 페이지

---
+++ src/Routes/PublicRoute.js
@@ -0,0 +1,14 @@
+import { Route } from 'react-router-dom';
+
+const PublicRoute = ({ component: Component, restricted, ...rest }) => {
+  return (
+    <Route
+    // {...rest}
+    // render={(props) =>
+    //   isLogin() && restricted ? <Redirect to="/" /> : <Component {...props} />
+    // }
+    />
+  );
+};
+
+export default PublicRoute;
