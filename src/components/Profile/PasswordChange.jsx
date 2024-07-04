5793fc2d5660eafc35ac0809627f21f2c709e1d3
seeunchung <jjssee210@gmail.com>
2024-07-04 18:14:52 +0900

    Feat: 어드민 프로필조회, 비밀번호 변경

--- src/components/Profile/PasswordChange.jsx
+++ src/components/Profile/PasswordChange.jsx
@@ -73,7 +73,7 @@ const PasswordChange = ({ endpoint }) => {
     }
 
     try {
-      const response = await apiClient.patch({ endpoint }, formData);
+      const response = await apiClient.patch(endpoint, formData);
 
       if (response.status === 200) {
         alert('비밀번호가 성공적으로 변경되었습니다. 다시 로그인 해주세요');
