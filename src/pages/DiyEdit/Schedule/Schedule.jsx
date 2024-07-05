ffca6b0ee03ff27e00b04e98fae83e3eb6034661
seeunchung <jjssee210@gmail.com>
2024-07-05 23:51:53 +0900

    Feat: diy작성 일부 수정

--- src/pages/DiyEdit/Schedule/Schedule.jsx
+++ src/pages/DiyEdit/Schedule/Schedule.jsx
@@ -81,7 +81,7 @@ const Schedule = () => {
               {detail.courses.map((course, i) => (
                 <li key={`course-${i}`}>
                   <div className={styles.detail}>
-                    <p>{`course ${i + 1}`}</p>
+                    <p>{`코스 ${i + 1}`}</p>
                     {detail.fileUrls[i] ? (
                       <img src={detail.fileUrls[i]} alt="코스 이미지" />
                     ) : (
