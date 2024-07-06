6b61f0abcc10ae0c5261c8fb0500883089bc197a
seeunchung <jjssee210@gmail.com>
2024-07-06 15:37:11 +0900

    Feat:diy디테일 디자인 1차

--- src/components/Diy/DiySchedule.jsx
+++ src/components/Diy/DiySchedule.jsx
@@ -7,22 +7,30 @@ const CourseList = ({ detaileCourses }) => {
     <ul className={styles.container}>
       {detaileCourses.map((detail, index) => (
         <li key={index} className={styles.course_box}>
-          <div className={styles.date_and_button}>
+          <div className={styles.date_box}>
             <h4>{detail.dayNum}</h4>
+            <p>{index + 1}일 차</p>
           </div>
           <ul className={styles.courses}>
             {detail.courses.map((course, i) => (
-              <li key={`course-${i}`}>
-                <div className={styles.detail}>
-                  <p>{`코스 ${i + 1}`}</p>
-                  {detail.fileUrls[i] ? (
-                    <img src={detail.fileUrls[i]} alt="코스 이미지" />
-                  ) : (
-                    <PlaceIcon sx={{ m: 2 }} />
-                  )}
-                  <p>{course}</p>
-                </div>
-              </li>
+              <React.Fragment key={`course-fragment-${i}`}>
+                <li key={`course-${i}`}>
+                  <div className={styles.detail}>
+                    <p>{`코스 ${i + 1}`}</p>
+                    {detail.fileUrls[i] ? (
+                      <img src={detail.fileUrls[i]} alt="코스 이미지" />
+                    ) : (
+                      <PlaceIcon sx={{ m: 2 }} />
+                    )}
+                    <p data-full-text={course}>{course}</p>
+                  </div>
+                </li>
+                {i < detail.courses.length - 1 && (
+                  <svg key={`line-${i}`} className={styles.line}>
+                    <line x1="0" y1="149" x2="200" y2="149" />
+                  </svg>
+                )}
+              </React.Fragment>
             ))}
           </ul>
         </li>
--- src/pages/DiyDetail/DiyDetailSchedule/DiyDetailSchedule.jsx
+++ src/pages/DiyDetail/DiyDetailSchedule/DiyDetailSchedule.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import styles from '../DiyDetail.module.css';
 import PlaceIcon from '@mui/icons-material/Place';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import CourseList from '../../../components/Diy/DiySchedule';
 
 const DiyDetailSchedule = ({ schedule }) => {
   // 상태 객체를 사용하여 각 detailCourseNum별 이미지 보이기 상태를 관리
@@ -16,70 +17,10 @@ const DiyDetailSchedule = ({ schedule }) => {
   };
 
   return (
-    <>
+    <div>
       <h3>여행 일정</h3>
-      <div className={styles.schedule_box}>
-        {schedule &&
-          schedule.map((item) => {
-            // courses와 fileUrls 배열을 병합하여 새로운 배열 생성
-            const courseDetails = item.courses.map((course, index) => ({
-              course,
-              fileUrl: item.fileUrls[index],
-            }));
-
-            return (
-              <div key={item.detailCourseNum}>
-                <div className={styles.day_course_box}>
-                  <div className={styles.day_box}>
-                    <span>{item.dayNum}</span>
-                  </div>
-                  <div
-                    className={styles.course_box}
-                    onClick={() => handleOpenImg(item.detailCourseNum)}
-                  >
-                    {item.courses.map((course, index) => (
-                      <div key={index}>{`${index + 1} : ${course}`}</div>
-                    ))}
-
-                    <KeyboardArrowDownIcon className={styles.icon} />
-                  </div>
-                </div>
-                {/* openImgStates 상태가 true일 때만 courseDetails를 렌더링 */}
-                {openImgStates[item.detailCourseNum] && (
-                  <div className={styles.imgs_box}>
-                    {courseDetails.reduce((acc, detail, index) => {
-                      acc.push(
-                        <div
-                          key={`detail-${index}`}
-                          className={styles.img_desc}
-                        >
-                          {detail.fileUrl ? (
-                            <img src={detail.fileUrl} alt={`Image ${index}`} />
-                          ) : (
-                            <PlaceIcon sx={{ m: 2 }} />
-                          )}
-                          <div className={styles.course_name}>
-                            {`${detail.course}`}
-                          </div>
-                        </div>
-                      );
-                      // 각 인덱스 사이에 SVG 태그 삽입
-                      if (index < courseDetails.length - 1) {
-                        acc.push(
-                          <svg key={`line-${index}`} className={styles.line}>
-                            <line x1="0" y1="118" x2="800" y2="118" />
-                          </svg>
-                        );
-                      }
-                      return acc;
-                    }, [])}
-                  </div>
-                )}
-              </div>
-            );
-          })}
-      </div>
-    </>
+      <CourseList detaileCourses={schedule} />
+    </div>
   );
 };
 
