91be3d063f4c3083b3a0f6dd8bf74150fa19f0c4
seeunchung <jjssee210@gmail.com>
2024-07-09 10:33:58 +0900

    Feat: diy디자인 수정

--- src/pages/DiyDetail/DiyDetailSchedule/DiyDetailSchedule.jsx
+++ src/pages/DiyDetail/DiyDetailSchedule/DiyDetailSchedule.jsx
@@ -1,7 +1,3 @@
-import { useState } from 'react';
-import styles from '../DiyDetail.module.css';
-import PlaceIcon from '@mui/icons-material/Place';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import CourseList from '../../../components/Diy/DiySchedule';
 
 const DiyDetailSchedule = ({ schedule }) => {
