47af9291bc5fe6c331eb85ffa0ba54c2a529e8dc
seeunchung <jjssee210@gmail.com>
2024-07-10 02:00:53 +0900

    Feat: 여행 출발일 지난 표시, 버튼 막기 추가

--- src/components/Diy/DiyCard.jsx
+++ src/components/Diy/DiyCard.jsx
@@ -1,62 +1,71 @@
-  import { useNavigate } from 'react-router-dom';
-  import PropTypes from 'prop-types';
-  import styles from './DiyCard.module.css';
-  import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-  import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import { useNavigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import styles from './DiyCard.module.css';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 
-  const DiyCard = ({
-    packageNum,
-    user,
-    profileImg,
-    packageName,
-    shortDescription,
-    packageLikedNum,
-    airline
-  }) => {
-    const navigate = useNavigate();
+const DiyCard = ({
+  packageNum,
+  user,
+  profileImg,
+  packageName,
+  shortDescription,
+  packageLikedNum,
+  airline,
+}) => {
+  const navigate = useNavigate();
 
-    const onClickDiyDetail = () => {
-      navigate(`/diy/${packageNum}`);
+  const onClickDiyDetail = () => {
+    navigate(`/diy/${packageNum}`);
+  };
 
-      
-    };
+  // 현재 날짜 가져오기
+  const currentDate = new Date();
 
-    return (
-      <div className={styles.DiyCard} onClick={onClickDiyDetail}>
-        <div className={styles.diy_card_thumbnail}>
-          <img src={profileImg} alt={`${packageName} 썸네일`} />
+  // boardingDate와 비교하여 이미 지난 날짜인지 확인
+  const isPastDate = new Date(airline.boardingDate) < currentDate;
+
+  return (
+    <div
+      className={`${styles.DiyCard} ${isPastDate ? styles.pastDate : ''}`}
+      onClick={onClickDiyDetail}
+    >
+      <div className={styles.diy_card_thumbnail}>
+        <img src={profileImg} alt={`${packageName} 썸네일`} />
+      </div>
+      <div className={styles.text_box}>
+        <h4>{packageName}</h4>
+        <div className={styles.desc}>{shortDescription}</div>
+      </div>
+      <div className={styles.grayDetail}>
+        <div>
+          여행기간 | {new Date(airline.boardingDate).toLocaleDateString()}~
+          {new Date(airline.comingDate).toLocaleDateString()}
         </div>
-        <div className={styles.text_box}>
-          <h4>{packageName}</h4>
-          <div className={styles.desc}>{shortDescription}</div>
-        </div>
-        <div className={styles.grayDetail}>
-          <div>여행기간 | {new Date(airline.boardingDate).toLocaleDateString()}~{new Date(airline.comingDate).toLocaleDateString()}</div>
-        
 
-          <div className={styles.likes}>
-          <ThumbUpIcon fontSize='0.1rem' className={styles.thumb_icon} />
+        <div className={styles.likes}>
+          <ThumbUpIcon fontSize="0.1rem" className={styles.thumb_icon} />
           <span>{packageLikedNum}</span>
         </div>
-          <div className={styles.user_box}>
-          <AccountCircleIcon fontSize='1rem' />
+        <div className={styles.user_box}>
+          <AccountCircleIcon fontSize="1rem" />
           <div>{user.userNickname}</div>
         </div>
-        </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
-  DiyCard.propTypes = {
-    packageNum: PropTypes.number.isRequired,
-    page: PropTypes.bool,
-    user: PropTypes.shape({
-      userName: PropTypes.string.isRequired,
-    }).isRequired,
-    profileImg: PropTypes.string,
-    packageName: PropTypes.string.isRequired,
-    shortDescription: PropTypes.string,
-    // children: PropTypes.element.isRequired,
-  };
+DiyCard.propTypes = {
+  packageNum: PropTypes.number.isRequired,
+  page: PropTypes.bool,
+  user: PropTypes.shape({
+    userName: PropTypes.string.isRequired,
+  }).isRequired,
+  profileImg: PropTypes.string,
+  packageName: PropTypes.string.isRequired,
+  shortDescription: PropTypes.string,
+  // children: PropTypes.element.isRequired,
+};
 
-  export default DiyCard;
+export default DiyCard;
--- src/pages/DiyDetail/DiyDetail.jsx
+++ src/pages/DiyDetail/DiyDetail.jsx
@@ -51,6 +51,12 @@ const DiyDetail = () => {
     return <div>데이터가 없습니다.</div>;
   }
 
+  // 현재 날짜 가져오기
+  const currentDate = new Date();
+
+  // boardingDate와 비교하여 이미 지난 날짜인지 확인
+  const isPastDate = new Date(airline.boardingDate) < currentDate;
+
   return (
     <>
       <Metas title={desc.packageName} />
@@ -84,7 +90,9 @@ const DiyDetail = () => {
                 <VisibilityIcon />
                 <div>{data.viewNum}</div>
               </div>
-              {isWriter ? ( // 작성자일 경우 렌더링
+              {isPastDate ? (
+                <button className={styles.cheer_button}>응원 만료</button>
+              ) : isWriter ? (
                 <button className={styles.cheer_button}>응원 받는 중!</button>
               ) : (
                 <CheerButton
